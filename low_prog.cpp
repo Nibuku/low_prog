e208b5c2413f3ccc728b0a0b8c347c88dbd5f295
Вероника Будюк <veronika.budyuk2004@gmail.com>
2024-09-17 14:51:21 +0400

    Сделан генератор xorshift128 на Си

--- low_prog.cpp
+++ low_prog.cpp
@@ -1,12 +1,35 @@
-﻿// low_prog.cpp: определяет точку входа для приложения.
-//
+﻿// Вариант 5
+// Реализовать генератор псевдослучайных чисел xorshift128. 
+// Используя введенное пользователем число в качестве начальной точки, вывести 100 сгенерированных чисел.
 
-#include "low_prog.h"
+#include <stdio.h>
+//#include <stdint.h>
 
-using namespace std;
+int array[100];
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	int seed[4]; //88675123
+	scanf("%d", &seed[0]);
+	seed[1] = 123456789;
+	seed[2] = 362436069;
+	seed[3] = 521288629;
+
+	unsigned n;
+	scanf("%u", &n);
+
+	for (int i = 0; i < n; ++i) {
+		int t = seed[3];
+		int s = seed[0];
+
+		seed[3] = seed[2];
+		seed[2] = seed[1];
+		seed[1] = s;
+
+		t ^= t << 11;
+		t ^= t >> 8;
+		seed[0] = t ^ s ^ (s >> 19);
+		printf("%d ", seed[0]);
+	}
 	return 0;
 }
