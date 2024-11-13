d841947770b197dad428193de61c87da7c0eb1a5
Вероника Будюк <veronika.budyuk2004@gmail.com>
2024-11-13 21:26:50 +0400

    Last version

--- lab2-task2.asm
+++ lab2-task2.asm
@@ -1,12 +1,17 @@
+section .data
+max_: dd 20
+
 
 section .rodata
-value: dd 20.0
+
+value: dd 2.6
 a: dd 1.0
-two_pi: dd 6.28319
-factorials: dd 2.0, 24.0, 720.0,40320.0,3628800.0, \
-            479001600.0, 87178291200.0, 20922789888000.0, \
-            6402373705728000.0, 2432902008176640000.0,
-               
+
+min_one: dd -1.0
+two: dd 2.0
+three: dd 3.0
+two_pi: dd 6.28318530
+  
 section .bss
 x: resd 1
 
@@ -17,74 +22,47 @@ cos:
     
     movss xmm0, [value] 
     movss xmm4, [two_pi]
+    movss xmm8, [a]
 .cycle_start: 
     comiss xmm0,xmm4
-    ja .cycle_end
-    
-
+    ja .cycle_sub
     
     mulss xmm0, xmm0 ;x^2
-    movss xmm1, xmm0  
-    movss xmm2, xmm0 ; для последующ. возв. в степень
-    movss xmm3, [a]
-    divss xmm1, [factorials] ; x^2/2!
-    subss xmm3, xmm1 ; a-x^2/2!
+    mulss xmm0, [min_one] ;-x^2
     
+    movss xmm3, [a] ;1 - нулевой член
+    mov ecx, 1
+.cycle_cos:   
+    cmp ecx, [max_]
+    je .cycle_end
+    
+    mov eax, ecx 
+    shl eax, 1   
+    mov edx, eax 
+    sub eax, 1   
+    mul edx      
+
+    cvtsi2ss xmm9, eax
+    movss xmm10, xmm0
+    divss xmm10, xmm9 ;-x^2/2n(2n-1)
+    mulss xmm8, xmm10 ;
+
+    addss xmm3, xmm8
 
     
-    mulss xmm2, xmm0 ;x^4
-    movss xmm1, xmm2
-    divss xmm1, [factorials+4] ;x^4/4!
-    addss xmm3, xmm1
+    add ecx, 1
+    jmp .cycle_cos
+
     
-    mulss xmm2, xmm0 ; x^6
-    movss xmm1, xmm2
-    divss xmm1, [factorials+8]
-    subss xmm3, xmm1
-    
-    mulss xmm2, xmm0 ;x^8
-    movss xmm1, xmm2
-    divss xmm1, [factorials+12]
-    addss xmm3, xmm1
-    
-    mulss xmm2, xmm0 ;x^10
-    movss xmm1, xmm2
-    divss xmm1, [factorials+16]
-    subss xmm3, xmm1
-    
-    mulss xmm2, xmm0 ;x^12
-    movss xmm1, xmm2
-    divss xmm1, [factorials+20]
-    addss xmm3, xmm1
-    
-    mulss xmm2, xmm0 ;x^14
-    movss xmm1, xmm2
-    divss xmm1, [factorials+24]
-    subss xmm3, xmm1
-    
-    mulss xmm2, xmm0 ;x^16
-    movss xmm1, xmm2
-    divss xmm1, [factorials+28]
-    addss xmm3, xmm1
-    
-    mulss xmm2, xmm0 ;x^18
-    movss xmm1, xmm2
-    divss xmm1, [factorials+32]
-    subss xmm3, xmm1
-    
-    mulss xmm2, xmm0 ;x^20
-    movss xmm1, xmm2
-    divss xmm1, [factorials+36]
-    addss xmm3, xmm1
-    
-    movss [x], xmm3
-    ret 
-    
-.cycle_end:
+.cycle_sub:
     subss xmm0, xmm4
     jmp .cycle_start
+.cycle_end:
+    movss [x], xmm3
+    xor rax, rax
+    ret
     
 main:
     call cos
-    xor rax, rax
+    xor eax, eax
     ret
